c09f4e72931aa3c14ec67ba4dbbe601ec70999b9
GonzOr <helice.uno@gmail.com>
2023-04-17 20:44:59 +0200

    database4

--- prueba/settings.py
+++ prueba/settings.py
@@ -90,7 +90,12 @@ WSGI_APPLICATION = 'prueba.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {'default': dj_database_url.config(default='postgres://user:pass@localhost/dbname')}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
